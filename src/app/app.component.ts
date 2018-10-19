4ace60e4ec3617cda3a6565c1a29f3ba05a88553
Jad <jad.chahine4@gmail.com>
2018-10-19 11:29:58 +0300

    Add an email input and show it's reverse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Training';
+  title = 'Angular Training';
+
+  myform: FormGroup;
+  email: FormControl;
+  emailString: string;
+  reverseEmail: string;
+
+  constructor(fb: FormBuilder){
+
+    this.email = new FormControl();
+
+    this.myform = fb.group({
+      email: this.email
+    });
+
+    this.email.valueChanges.subscribe( item => {
+      this.emailString = item;
+      this.reverseEmail = item.split('').reverse().join('');
+    })
+
+  }
+
 }
